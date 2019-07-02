cf943baba641dc3286b68b171e395bf5a41cf801
jooyg62 <jooyg62@naver.com>
2019-07-02 16:30:46 +0900

    sys path 2차 commit

--- python_ch3/wsgi.py
+++ python_ch3/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 import sys
 
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-if path not in sys.path:
-    sys.path.append(path)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
